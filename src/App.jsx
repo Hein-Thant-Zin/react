7938f9ecc99174ca82b69226b6eeaaa834fe19f4
heinthantzin <heintz414@gmail.com>
2023-08-22 15:10:49 +0630

    using axios

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useCallback, useEffect, useReducer, useState } from "react";
 import InputWithLabel from "./components/inPutWithLabel";
 import Articlelist from "./components/ArticleList";
 import useStorageState from "./hooks/useStorageState";
+import axios from "axios";
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -63,12 +64,11 @@ export default function App() {
      if (!searchTerm) return;
     // setIsLoading(true);
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
-    fetch(url)
-      .then(res => res.json())
+    axios.get(url)
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,     
+          payload: result.data.hits,     
       })
       }).catch(() => {
         dispatchStories({
