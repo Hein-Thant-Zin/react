8a9462af82abf7f61896556fe38b5a43af487684
heinthantzin <heintz414@gmail.com>
2023-08-23 15:23:44 +0630

    using SVG

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
-import { useCallback, useEffect, useReducer, useState } from "react";
+import { useCallback, useEffect, useMemo, useReducer, useState } from "react";
 import InputWithLabel from "./components/inPutWithLabel";
 import Articlelist from "./components/ArticleList";
 import useStorageState from "./hooks/useStorageState";
 import axios from "axios";
 import './App.css';
+
 
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
@@ -61,9 +62,22 @@ export default function App() {
 
   });
   const [url, setUrl] = useState(`${API_ENDPOINT}${searchTerm}`);
+console.log('stories',stories.data);
+  function calculateSumofComments(items) {
+    return items.data.reduce((result, value) => result + value.num_comments, 0  
+    );
+  }
+  const sumOfComments = useMemo(()=>calculateSumofComments(stories),
+  [stories])
 
+  // const sumOfComments = calculateSumofComments(stories);
+//  console.log(sumOfComments);
+  
+  useEffect(() => {
+    console.log(sumOfComments);
+  },[stories])
   const handleFetch = useCallback(async() => {
-     if (!searchTerm) return;
+    //  if (!searchTerm) return;
     // setIsLoading(true);
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
@@ -92,13 +106,15 @@ export default function App() {
   const handleSearch=(event)=>  {
     setSearchTerm(event.target.value);
   }
-  const handleRemoveStory = (item) => {
-    // setStories(newStories);
-    dispatchStories({
-      type: 'REMOVE_STORY',
-      payload: item,
-    })
-  };
+  const handleRemoveStory = useCallback(
+    (item) => {
+      // setStories(newStories);
+      dispatchStories({
+        type: 'REMOVE_STORY',
+        payload: item,
+      })
+    }
+  );
   const handleSubmit = (event) => {
     event.preventDefault();
     setUrl(`${API_ENDPOINT}${searchTerm}`);
@@ -110,8 +126,11 @@ export default function App() {
       label='Search' 
       />
     <button type="submit" >Search</button>
-      </form>
+    </form>
+        <br />
     <hr />
+        <br />
+
     {stories.isError ?<p>Something went wrong!!</p>:null}
     
     {stories.isLoading  ? (<p>Loading...</p>) : ( <Articlelist list={stories.data}
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,9 +1,20 @@
-import './ArticleList.css'
+import './ArticleList.css';
+import { memo } from 'react';
+import { ReactComponent as Tick } from '../assets/tick.svg';
+
 // eslint-disable-next-line react/prop-types
-export default function Articlelist({list,handleRemoveStory}) {
+const Articlelist=memo(({list,handleRemoveStory}) =>{
   return (
      <ul className='item-container'>
       {/* eslint-disable-next-line react/prop-types*/}
+      <li style={{ display: 'flex', fontWeight:'700',alignItems:'center' ,fontSize:'20px',marginBottom:'1rem'}}>
+        <span style={{ width:"40%" }}>Title</span>
+      <span style={{ width:"30%" }}>Author Name</span>
+      <span style={{ width:"20%" }}>Comments</span>
+        <span style={{ width: "10%" }}>Points</span>
+        <span style={{ width: "10%" }}>Actions</span>
+        
+      </li>
       
       {list.length === 0?<p>There is no data!</p>:list.map((item) => 
          (
@@ -12,7 +23,7 @@ export default function Articlelist({list,handleRemoveStory}) {
       )}
       </ul>
   )
-}
+})
 // eslint-disable-next-line react/prop-types
 function Article({article,handleRemoveStory}) {
   const { url,title ,author,num_comments,points } = article;
@@ -22,11 +33,13 @@ function Article({article,handleRemoveStory}) {
       <span style={{ width:"40%" }}><a href={url}>{title}</a></span>
       <span style={{ width:"30%" }}>{author}</span>
       <span style={{ width:"20%" }}>{num_comments}</span>
-      <span style={{ width:"10%" }}>{points}</span>
-      <button className='button' style={{ width:"10%" }} onClick={()=>handleRemoveStory(article)}>Delete</button>
+      <span style={{ width: "10%" }}>{points}</span>
+     
+      <button  className='button'  style={{ width:"10% " }} onClick={()=>handleRemoveStory(article)}><Tick height='30px' width='50px' /></button>
     </li>
   )
   
 }
+export default Articlelist
 
 
